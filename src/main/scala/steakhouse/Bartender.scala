c081cdd8285e53c7e7b5dff443a0f97e905c82d4
slowikps <slowikps@gmail.com>
2018-04-07 15:41:33 +0100

    Ex3 Solution

--- src/main/scala/steakhouse/Bartender.scala
+++ src/main/scala/steakhouse/Bartender.scala
@@ -1,9 +1,17 @@
 package steakhouse
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success}
 
 class Bartender {
   import Utils._
 
-  def beer()(implicit ec: ExecutionContext): Future[String] = ??? //delay("beer") use Promise here instead
+  def beer()(implicit ec: ExecutionContext): Future[String] = {
+    val p = Promise[String]
+    delay("beer").onComplete {
+      case Success(success) => p.trySuccess(success)
+      case Failure(cause)   => p.tryFailure(cause)
+    }
+    p.future
+  }
 }
