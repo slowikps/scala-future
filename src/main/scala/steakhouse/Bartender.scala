12a92379c56099c8b78d18e9f839329abc387988
slowikps <slowikps@gmail.com>
2018-04-07 15:37:13 +0100

    Ex 3

--- src/main/scala/steakhouse/Bartender.scala
+++ src/main/scala/steakhouse/Bartender.scala
@@ -5,5 +5,5 @@ import scala.concurrent.{ExecutionContext, Future}
 class Bartender {
   import Utils._
 
-  def beer()(implicit ec: ExecutionContext): Future[String] = delay("beer")
+  def beer()(implicit ec: ExecutionContext): Future[String] = ??? //delay("beer") use Promise here instead
 }
