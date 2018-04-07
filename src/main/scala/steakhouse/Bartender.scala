ff2592e3896239fe4f05740bd6d2b005deaa2d49
slowikps <slowikps@gmail.com>
2018-04-07 15:11:35 +0100

    Ex2 - solution

--- src/main/scala/steakhouse/Bartender.scala
+++ src/main/scala/steakhouse/Bartender.scala
@@ -1,7 +1,9 @@
 package steakhouse
 
+import scala.concurrent.{ExecutionContext, Future}
+
 class Bartender {
   import Utils._
 
-  def beer(): String = delay("beer")
+  def beer()(implicit ec: ExecutionContext): Future[String] = delay("beer")
 }
